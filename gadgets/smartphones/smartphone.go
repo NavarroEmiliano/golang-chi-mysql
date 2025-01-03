4332daabc84b6bc078f9de527ce09e14786fee7a
Emiliano Navarro <emilianonavarro1993@gmail.com>
2025-01-03 13:14:46 -0300

    smartphone model

---
+++ gadgets/smartphones/smartphone.go
@@ -0,0 +1,10 @@
+package smartphones
+
+// Smartphone model structure for smartphones
+type Smartphone struct {
+	Id            int64 // similar to a long in Java
+	Name          string
+	Price         int
+	CountryOrigin string
+	Os            string
+}
